be66ef86e9658df54bf77910bcfe3cb7fcc2139d
Krishna Sarma <krishna.sarma@smartpipesolutions.com>
2019-05-24 11:42:08 +0100

    custom route table added

--- vpc.tf
+++ vpc.tf
@@ -49,3 +49,31 @@ resource "aws_internet_gateway" "gw" {
     Name = "main_gw"
   }
 }
+
+resource "aws_route_table" "custom_route_table" {
+  vpc_id = "${aws_vpc.wordpress_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "custom_ig_route_table"
+  }
+}
+
+resource "aws_route_table_association" "association_a" {
+  subnet_id      = "${aws_subnet.subnet_availability_zone_a.id}"
+  route_table_id = "${aws_route_table.custom_route_table.id}"
+}
+
+resource "aws_route_table_association" "association_b" {
+  subnet_id      = "${aws_subnet.subnet_availability_zone_b.id}"
+  route_table_id = "${aws_route_table.custom_route_table.id}"
+}
+
+resource "aws_route_table_association" "association_c" {
+  subnet_id      = "${aws_subnet.subnet_availability_zone_c.id}"
+  route_table_id = "${aws_route_table.custom_route_table.id}"
+}
