7d330d7a9e192ff7a417a42e3f08c94cfbec9628
Krishna Sarma <krishna.sarma@smartpipesolutions.com>
2019-05-31 09:14:04 +0100

    removed security group from LB and added to instances.

--- elb.tf
+++ elb.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "wordpress_application_loadbalancer" {
+resource "aws_lb" "wordpress_network_loadbalancer" {
   name               = "wordpress-loadbalancer"
   internal           = false
   load_balancer_type = "network"
@@ -18,8 +18,8 @@ resource "aws_lb_target_group" "wordpress_instance_target" {
   vpc_id   = "${aws_vpc.wordpress_vpc.id}"
 }
 
-resource "aws_lb_listener" "wordpress_lb_listener" {
-  load_balancer_arn = "${aws_lb.wordpress_application_loadbalancer.arn}"
+resource "aws_lb_listener" "wordpress_http_lb_listener" {
+  load_balancer_arn = "${aws_lb.wordpress_network_loadbalancer.arn}"
   port              = "80"
   protocol          = "TCP"
 
@@ -28,6 +28,19 @@ resource "aws_lb_listener" "wordpress_lb_listener" {
     target_group_arn = "${aws_lb_target_group.wordpress_instance_target.arn}"
   }
 }
+
+resource "aws_lb_listener" "wordpress_https_lb_listener" {
+  load_balancer_arn = "${aws_lb.wordpress_network_loadbalancer.arn}"
+  port              = "443"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.wordpress_instance_target.arn}"
+  }
+}
+
+#changes when ASG is implemented
 
 resource "aws_lb_target_group_attachment" "wordpress_target_group_attachment" {
   target_group_arn = "${aws_lb_target_group.wordpress_instance_target.arn}"
