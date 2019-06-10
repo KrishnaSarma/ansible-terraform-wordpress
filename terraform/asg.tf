19e21c418392a5075d24676d08e18135a545f3d0
Krishna Sarma <krishna.sarma@smartpipesolutions.com>
2019-06-10 08:44:54 +0100

    changed ansible code to remove unnecesary files and only implement wordpress. changed terraform user data to included only whats needed.

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -57,7 +57,7 @@ resource "aws_launch_configuration" "wp_asg_launch_config" {
   associate_public_ip_address = "True"
   security_groups             = ["${aws_security_group.mgmt_instance_security_group.id}", "${aws_security_group.sg-internet-instances.id}"]
   key_name                    = "${aws_key_pair.wordpress_key_pair.key_name}"
-  user_data                   = "${file("user_data/user_data_test.sh")}"
+  user_data                   = "${file("user_data/bootstrap.sh")}"
 
   lifecycle {
     create_before_destroy = true
